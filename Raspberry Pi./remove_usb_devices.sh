a7eaaf6e6f4bbd978124f5b270f0b2cf2147015e
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-02 20:31:11 +0900

    Add files via upload

---
+++ Raspberry Pi./remove_usb_devices.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Run script as root
+if [ $(id -u) -ne 0 ]; then exec sudo bash "$0" "$@"; exit; fi
+
+#Change Directory to usb devices.
+cd /sys/bus/usb/devices
+ls
+
+#Find device's ID number what you want to remove
+device_id=`lsusb | grep -e Logitech | cut -c 24-27`
+
+#Find device's directory
+grep_directory=`grep ${device_id} */idVendor | cut -d"/" -f 1`
+
+sudo echo 0 > /sys/bus/usb/devices/${grep_directory}/bConfigurationValue
