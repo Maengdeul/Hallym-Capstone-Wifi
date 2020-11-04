f34e826bb2a91bda01fdff2ae8092350ae52fcb3
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-04 23:06:57 +0900

    Update ban_client_threshold.sh

--- OpenWRT/Shell-Script/ban_client_threshold.sh
+++ OpenWRT/Shell-Script/ban_client_threshold.sh
@@ -6,7 +6,6 @@
 # list all wireless network interfaces
 
 list=`iwinfo | grep ESSID | cut -f 1 -s -d" "`
-rx_limit=1
 cnt=0
 for interface in $list
 do
@@ -18,7 +17,7 @@ do
     tx=`iwinfo $interface assoclist | grep -i -A 4 $mac | grep TX | cut -f 2 -s -d" " | cut -d. -f 1`
     host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep -i $mac | cut -f 3 -s -d" "`
     # if client's rx over rx_limit, ban the client
-    if [ $rx -ge $rx_limit ] ; then
+    if [ $rx -ge $1 ] ; then
       ubus call hostapd.$interface del_client "{'addr':'$mac','reason':5,'deauth':false,'ban_time':10000}"
       echo -e "$((cnt+1)). $host has been banned"
       cnt=$((cnt+1))
