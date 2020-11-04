847d4e69b01b27f0804be7b2f46720ecac469a3f
wkdgjwodnd <57930607+wkdgjwodnd@users.noreply.github.com>
2020-11-04 23:05:58 +0900

    Update enter_mac_to_ban.sh

--- OpenWRT/Shell-Script/enter_mac_to_ban.sh
+++ OpenWRT/Shell-Script/enter_mac_to_ban.sh
@@ -7,17 +7,13 @@
 
 list=`iwinfo | grep ESSID | cut -f 1 -s -d" "`
 
-# Enter MAC addr
-echo -n "enter MAC addr : "
-read macaddr
-
 for interface in $list
 do
   maclist=`iwinfo $interface assoclist | grep dBm | cut -f 1 -s -d" "`
   for mac in $maclist
   do
   #Found match MAC address to ban
-    if [ ${macaddr} = ${mac} ]; then
+    if [ $1 = ${mac} ]; then
       ubus call hostapd.$interface del_client "{'addr':'$mac','reason':0,'deauth':false,'ban_time':500}"
       echo "client banned"
     fi
