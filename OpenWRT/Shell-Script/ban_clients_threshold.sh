947f054c301569a908ce37030c79655ec8746772
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-05 13:46:12 +0900

    Update ban_clients_threshold.sh

--- OpenWRT/Shell-Script/ban_clients_threshold.sh
+++ OpenWRT/Shell-Script/ban_clients_threshold.sh
@@ -16,8 +16,8 @@ do
     rx=`iwinfo $interface assoclist | grep -i -A 4 $mac | grep RX | cut -f 2 -s -d" " | cut -d. -f 1`
     tx=`iwinfo $interface assoclist | grep -i -A 4 $mac | grep TX | cut -f 2 -s -d" " | cut -d. -f 1`
     host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep -i $mac | cut -f 3 -s -d" "`
-    # if client's rx over rx_limit(first argument) and tx_limit(second argument) ban the client
-    if [ $rx -ge $1 ] && [ $tx -ge $2 ]; then
+    # if client's rx over rx_limit(first argument) or tx_limit(second argument) ban the client
+    if [ $rx -ge $1 ] || [ $tx -ge $2  ] ; then
       ubus call hostapd.$interface del_client "{'addr':'$mac','reason':5,'deauth':false,'ban_time':10000}"
       echo -e "$((cnt+1)). $host has been banned"
       cnt=$((cnt+1))
@@ -25,9 +25,9 @@ do
   done
 done
 if [ "$cnt" -eq 1 ] ; then
-  echo -e "1 client has been banned(RX=$rx, limit=$1 , $2)"
+  echo -e "1 client has been banned(RX=$rx, TX=$tx, RX/TX limit=$1, $2)"
 elif [ "$cnt" -gt 1 ] ; then
-  echo -e "$cnt clients have been banned(RX=$rx, limit=$1 , $2)"
+  echo -e "$cnt clients have been banned(RX=$rx, TX=$tx, RX/TX limit=$1, $2)"
 elif [ "$cnt" -eq 0 ] ; then
-  echo -e "No client has been banned(limit=$1 , $2)"
+  echo -e "No client has been banned(RX/TX limit=$1, $2)"
 fi
