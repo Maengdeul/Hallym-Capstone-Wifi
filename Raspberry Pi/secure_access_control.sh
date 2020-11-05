31e396e1f1c7f95ed947e417698a341d8ba6318b
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-05 17:36:55 +0900

    Add files via upload

---
+++ Raspberry Pi/secure_access_control.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# /etc/config/secure_access_control.sh
+# If connect AP, block camera
+
+ssid=`iwgetid | cut -f 2 -d":" | tr -d '"'`
+list="Dongwoo,Jaewoong"
+flag=0
+
+while :
+do
+  for i in $ssid
+  do
+    for j in ssid `echo ${list} | tr ',' '\n'`
+    do
+      if [ ${i} == ${j} ] ; then
+        flag=1
+      fi
+    done
+  done
+
+  if [ $flag == 1 ] ; then
+    /home/pi/rud.sh 1> /dev/null
+    xmessage -buttons OK:0 -nearmouse "Logitech devices off" -timeout 5 2> /dev/null
+  elif [ $flag == 0 ] ; then
+    /home/pi/lud.sh 1> /dev/null
+    xmessage -buttons Ok:0 -default Ok -nearmouse "Logitech devices on" -timeout 5 2> /dev/null
+  fi
+
+  sleep 5
+done
