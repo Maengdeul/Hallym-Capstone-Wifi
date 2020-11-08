29671aa896c70845592a58e9a565ca6afaf071e4
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-08 19:58:35 +0900

    Update redirect_clients.sh

--- Server/Shell-Script/redirect_clients.sh
+++ Server/Shell-Script/redirect_clients.sh
@@ -7,7 +7,7 @@
 # list all wireless network interfaces
 
 list=`iwinfo | grep ESSID | cut -f 1 -s -d" "`
-mlist="" # fill it up with your AP's MAC address
+mlist="" # Write your AP's MAC address
 
 for interface in $list
 do
