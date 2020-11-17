bc33438fe6d4d7df5e7e48f1173494331c3f7cb9
LHaEun <59590254+LHaEun@users.noreply.github.com>
2020-11-17 09:56:29 +0900

    Create run_ban_clients.sh

---
+++ Dashboard/Shell_Script/run_ban_clients.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MAC="$@"
+
+echo $MAC > /tmp/mac.txt
+
+#OpenWRT1(IP: 211.210.92.69)
+#mac address ban shell start
+ssh root@211.210.92.69 /etc/config/ban_clients.sh $MAC
+
+
+#OpenWRT2(IP: 211.210.92.69 -p1818)
+#mac address ban shell start
+#ssh root@211.210.92.69 -p1818 /etc/config/ban_clients.sh $MAC 
+
+
+#OpenWRT3(IP: 116.42.53.54 -p12333)
+#mac address ban shell start
+ssh root@116.42.53.54 -p 12333 "/root/test/ban_clients.sh $MAC"
+
+#OpenWRT4(IP: 116.42.53.54 -p12334) 
+#mac address ban shell start 
+#ssh root@116.42.53.54 -p12334 test/ban_clients.sh $MAC
+
+
