c8ec7e916bdd853c6a9eaf1a2b410c7dac08320e
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-23 00:13:31 +0900

    Update redirect_clients.sh

--- Server/redirect_clients.sh
+++ Server/redirect_clients.sh
@@ -7,7 +7,7 @@
 # list all wireless network interfaces
 
 list=`iwinfo | grep ESSID | cut -f 1 -s -d" "`
-mlist="" # Write your AP's MAC address
+mlist=`ifconfig -a | grep wlan | cut -c 39-`
 
 for interface in $list
 do
@@ -15,12 +15,23 @@ do
   for mac in $maclist
   do
     host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep -i $mac | cut -f 3 -s -d" "`
-    for i in `echo ${mlist} | tr ',' '\n'`
+    for i in $mlist
     do
-      if [ $1 == $mac -a $2 != ${i} ] ; then
-        ubus call hostapd.$interface del_client "{'addr':'$mac','reason':5,'deauth':false,'ban_time':10000}"
-        echo "$1 banned completely!(from ${i})"
+      if [ $1 == $mac -a $2 != $i ] ; then
+        ubus call hostapd.$interface del_client "{'addr':'$mac','reason':0,'deauth':false,'ban_time':60000}"
+        uci set wireless.default_radio1.macfilter=deny
+        uci add_list wireless.default_radio1.maclist=$1
+        uci commit wireless
+        luci-reload
+        echo "Handover $1 completely!(from ${i})"
       fi
     done
   done
 done
+
+sleep 1m
+
+uci del wireless.default_radio1.macfilter
+uci del wireless.default_radio1.maclist
+uci commit wireless
+luci-reload
