7c0afaeef9b92d6a921436843120af3e93e7c3ec
MengDeul <52237196+Maengdeul@users.noreply.github.com>
2020-11-23 00:13:51 +0900

    Update ban_clients.sh

--- OpenWRT/Shell-Script/ban_clients.sh
+++ OpenWRT/Shell-Script/ban_clients.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # /etc/config/ban_clients.sh
-# Ban the entered MAC address
+# Set the limit, ban time  and ban clients who over the limit
 
 # list all wireless network interfaces
 
@@ -11,11 +11,22 @@ for interface in $list
 do
   maclist=`iwinfo $interface assoclist | grep dBm | cut -f 1 -s -d" "`
   for mac in $maclist
+  # for each interface, get RX/TX of connected stations/clients
   do
-  #Found match MAC address to ban
-    if [ $1 == $mac ]; then
-      ubus call hostapd.$interface del_client "{'addr':'$mac','reason':0,'deauth':false,'ban_time':500}"
+    if [ $1 == ${mac} ]; then
+      ubus call hostapd.$interface del_client "{'addr':'$mac','reason':0,'deauth':false,'ban_time':60000}"
+      uci set wireless.default_radio1.macfilter=deny
+      uci add_list wireless.default_radio1.maclist=$1
+      uci commit wireless
+      luci-reload
       echo "client banned"
     fi
   done
 done
+
+sleep 1m
+
+uci del wireless.default_radio1.macfilter
+uci del wireless.default_radio1.maclist
+uci commit wireless
+luci-reload
